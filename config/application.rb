64623217344cf55c2aad31e3a51514bacd33da3c
Kevin Tran <trank.kevin@gmail.com>
2017-06-04 12:02:06 -0700

    Update rails generator config

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,14 @@ module NotedApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    #to skip assets, scaffolds.css, test framework, helpers, view
+    config.generators do |g|
+      g.template_engine nil #to skip views
+      g.test_framework  nil #to skip test framework
+      g.assets  false
+      g.helper false
+      g.stylesheets false
+    end
+
   end
 end
